ea98ef735f4069d8e490e6cca0f800e596e3b9d3
olaalao <ooa130330@utdallas.edu>
2015-05-04 13:21:04 -0500

    Update app.rb

--- app.rb
+++ app.rb
@@ -3,7 +3,7 @@ require 'sinatra'
 set :bind, '0.0.0.0'
 set :port,8080
 get '/' do
-  "Team 10 Rebuild image!\n"+
+  "Hello! Rebuild image!\n"+
 # ENV values are generated during template processing
 # and then passed to the container when openshift launches it.
   "User is #{ENV['ADMIN_USERNAME']}\n"+
